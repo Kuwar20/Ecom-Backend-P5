dad27e42f3a3a9a5688eba41c3378716ab1b1815
Kuwar20 <kuwarx1@gmail.com>
2024-01-13 20:19:05 +0530

    Add error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoute from './routes/user.js';
 import { connectDB } from './utils/features.js';
+import { errorMiddleware } from './middlewares/error.js';
 const app = express();
 const port = 3000;
 
@@ -16,6 +17,8 @@ res.send("API is working with /api/v1")
 }) */
 
 app.use("/api/v1/user", userRoute);
+
+app.use(errorMiddleware);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,13 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
+import ErrorHandler from "../utils/utility-class.js";
+import { TryCatch } from "../middlewares/error.js";
 
-export const newUser = async (
-    req: Request<{}, {}, NewUserRequestBody>,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
+export const newUser = TryCatch(
+    async (
+        req: Request<{}, {}, NewUserRequestBody>,
+        res: Response,
+        next: NextFunction
+    ) => {
         const { name, email, photo, gender, _id, dob } = req.body;
         const user = await User.create({
             name,
@@ -17,8 +19,6 @@ export const newUser = async (
             _id,
             dob: new Date(dob),
         });
-        res.status(200).json({ success: true, message: `Welcome, ${user.name}` });
-    } catch (error) {
-        res.status(200).json({ success: false, message: error });
+        return res.status(201).json({ success: true, message: `Welcome, ${user.name}`, });
     }
-};
+);
