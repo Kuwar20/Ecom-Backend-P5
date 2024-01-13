472e80999661da2d6d5b8e2d0b4e00eb07c97e09
Kuwar20 <kuwarx1@gmail.com>
2024-01-13 15:54:45 +0530

    Add user route and database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import userRoute from './routes/user.js';
+import { connectDB } from './utils/features.js';
 const app = express();
 const port = 3000;
 
 
+app.use(express.json());
 
+connectDB();
 
+/* 
+// test api
+app.get("/", (req, res) => {
+res.send("API is working with /api/v1")
+}) */
 
+app.use("/api/v1/user", userRoute);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
---
+++ src/controllers/user.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { User } from "../models/user.js";
+import { NewUserRequestBody } from "../types/types.js";
+
+export const newUser = async (
+    req: Request<{}, {}, NewUserRequestBody>,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { name, email, photo, gender, _id, dob } = req.body;
+        const user = await User.create({
+            name,
+            email,
+            photo,
+            gender,
+            _id,
+            dob: new Date(dob),
+        });
+        res.status(200).json({ success: true, message: `Welcome, ${user.name}` });
+    } catch (error) {
+        res.status(200).json({ success: false, message: error });
+    }
+};
