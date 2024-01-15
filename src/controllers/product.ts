451a2f39bfebd248f66518854c5cf070afd47df2
Kuwar20 <kuwarx1@gmail.com>
2024-01-15 23:12:44 +0530

    Refactor product controller functions

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -14,8 +14,8 @@ export const newProduct = TryCatch(
 
         if (!name || !price || !category || !stock) {
             rm(photo.path, () => {
-                console.log("photo deleted")
-            })
+                console.log("photo deleted");
+            });
         }
         await Product.create({
             name,
@@ -29,36 +29,84 @@ export const newProduct = TryCatch(
             success: true,
             message: "Product created successfully",
         });
+    }
+);
+
+export const getlatestProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
+    return res.status(200).json({
+        success: true,
+        products,
     });
+});
 
+export const getAllCategories = TryCatch(async (req, res, next) => {
+    const categories = await Product.distinct("category");
+    return res.status(200).json({
+        success: true,
+        categories,
+    });
+});
 
-export const getlatestProducts = TryCatch(
-    async (req, res, next) => {
+export const getAdminProducts = TryCatch(async (req, res, next) => {
+    const products = await Product.find({});
 
-        const products = await Product.find({}).sort({ createdAt: -1 }).limit(5);
-        return res.status(200).json({
-            success: true,
-            products,
+    return res.status(200).json({
+        success: true,
+        products,
+    });
+});
+
+export const getSingleProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    return res.status(200).json({
+        success: true,
+        product,
+    });
+});
+
+export const updateProduct = TryCatch(async (req, res, next) => {
+    const { id } = req.params;
+    const { name, price, category, stock } = req.body;
+    const photo = req.file;
+    const product = await Product.findById(id);
+
+    if (!product) return next(new ErrorHandler("Product not found", 404));
+
+    if (photo) {
+        rm(product.photo, () => {
+            console.log("old photo deleted");
         });
+        product.photo = photo.path;
+    }
+
+    if (name) product.name = name;
+    if (price) product.price = price;
+    if (stock) product.stock = stock;
+    if (category) product.category = category;
+
+    await product.save();
+
+    return res.status(200).json({
+        success: true,
+        message: "Product Updated Successfully",
+    });
+});
+
+export const deleteProduct = TryCatch(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) return next(new ErrorHandler("Product not found", 404));
+
+    rm(product.photo, () => {
+        console.log("photo deleted");
     });
 
-export const getAllCategories = TryCatch(
-    async (req, res, next) => {
+    await product.deleteOne();
 
-        const categories = await Product.distinct("category");
-        return res.status(200).json({
-            success: true,
-            categories,
-        });
+    return res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully",
     });
-
-    export const getAdminProducts = TryCatch(
-    async (req, res, next) => {
-
-        const products = await Product.find({});
-
-        return res.status(200).json({
-            success: true,
-            products,
-        });
-    });
+});
